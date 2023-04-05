0986eb22a9e16e18ad2c27ff1ee6bffb5b1883f6
nazirulhziq <61816543+nazirulhziq@users.noreply.github.com>
2023-04-06 01:31:16 +0800

    connection update

--- Admin/conn.php
+++ Admin/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$db=mysqli_connect("localhost","root","","onefarm");
+	$db=mysqli_connect("us-cdbr-east-06.cleardb.net","be1e7880320ee9","63c35bd6","heroku_8d9f86b3e1930ef");
 	if(!$db){
 		die("Connection failed".mysqli_connect_error());
 	}
--- Admin/database/constants.php
+++ Admin/database/constants.php
@@ -1,10 +1,10 @@
 <?php
 
 
-define("HOST","localhost");
-define("USER","root");
-define("PASS","");
-define("DB","onefarm");
+define("HOST","us-cdbr-east-06.cleardb.net");
+define("USER","be1e7880320ee9");
+define("PASS","63c35bd6");
+define("DB","heroku_8d9f86b3e1930ef");
 
 define("DOMAIN","http://localhost/multimedia/Admin");
 
